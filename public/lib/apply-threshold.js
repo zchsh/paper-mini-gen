05110f6a14ab8962ce11e3fa23bb2eb1b071f680
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2025-04-08 08:26:06 -0400

    feat: add pad before blur

--- public/lib/apply-threshold.js
+++ public/lib/apply-threshold.js
@@ -9,9 +9,17 @@
  */
 async function applyThreshold(imgSrc, threshold, radius) {
 	let image = await IJS.Image.load(imgSrc);
-	return image
-		.gaussianFilter({ radius })
-		.grey()
-		.mask({ threshold })
-		.toDataURL();
+	return (
+		image
+			// Pad the image, to avoid blur issues near image boundaries
+			.pad({ size: radius * 2, algorithm: "set", color: [255, 255, 255, 255] })
+			// Apply the blur
+			.gaussianFilter({ radius })
+			// Convert to grayscale
+			.grey()
+			// Convert to black-and-white using a threshold mask
+			.mask({ threshold })
+			// Cast to a data URL that can be used as an <img> src
+			.toDataURL()
+	);
 }
