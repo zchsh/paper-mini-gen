c34e66187d6f10b486f3b7508ae185ced83979d8
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2025-04-11 17:29:50 -0400

    chore: stub in render-polygon-points-string file

---
+++ public/lib/render-polygon-points-string.js
@@ -0,0 +1,27 @@
+/**
+ *
+ * - a region is an array of points
+ *
+ * @param {{ regions: [number, number][][], inverted: boolean }} polygon
+ * @param {number} scale
+ * @returns
+ */
+function renderPathsString(polygon, scale = 1) {
+	// TODO: implement
+	// var svgpath = "",
+	// 	i,
+	// 	j;
+	// for (i = 0; i < paths.length; i++) {
+	// 	for (j = 0; j < paths[i].length; j++) {
+	// 		if (j === 0) {
+	// 			svgpath += "M";
+	// 		} else {
+	// 			svgpath += "L";
+	// 		}
+	// 		svgpath += paths[i][j].X / scale + ", " + paths[i][j].Y / scale;
+	// 	}
+	// 	svgpath += "Z";
+	// }
+	// if (svgpath == "") svgpath = "M0,0";
+	// return svgpath;
+}
