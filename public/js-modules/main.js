051b4a7feb3f2a535fd7a98daae40ab0d2b4c183
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2025-04-26 08:27:00 -0400

    feat: implement union step

--- public/js-modules/main.js
+++ public/js-modules/main.js
@@ -307,8 +307,44 @@ window.arrangeForUnion = arrangeForUnion;
  *
  * TODO: finish and clean up below
  */
-function applyUnion() {
-	console.log("TODO: implement the applyUnion	 function");
+function applyUnion(polygonObjs, renderId, debugId01, debugId02) {
+	console.log({ polygonObjs });
+
+	// Debug individual shapes
+	if (debugId01) {
+		const container = document.getElementById(debugId01);
+		container.innerHTML = "";
+		for (const polygonObj of polygonObjs) {
+			const svgString = renderPolygonsAsPathSvg([polygonObj], 9);
+			container.innerHTML += svgString;
+		}
+	}
+
+	// Debug arrangement of individual shapes
+	if (debugId02) {
+		const container = document.getElementById(debugId02);
+		const svgString = renderPolygonsAsPathSvg(polygonObjs, 9);
+		container.innerHTML = svgString;
+	}
+
+	/**
+	 * Union of many shapes, as pulled from examples:
+	 * https://github.com/velipso/polybooljs?tab=readme-ov-file#advanced-example-1
+	 */
+	const unionPolygonObj = unionPolygonObjects(polygonObjs);
+	const unionSvg = renderPolygonsAsPathSvg([unionPolygonObj], 9);
+	document.getElementById(renderId).innerHTML = unionSvg;
+	return unionPolygonObj;
+}
+
+function unionPolygonObjects(polygons) {
+	var segments = PolyBool.segments(polygons[0]);
+	for (var i = 1; i < polygons.length; i++) {
+		var seg2 = PolyBool.segments(polygons[i]);
+		var comb = PolyBool.combine(segments, seg2);
+		segments = PolyBool.selectUnion(comb);
+	}
+	return PolyBool.polygon(segments);
 }
 
 window.applyUnion = applyUnion;
