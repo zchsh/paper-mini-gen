686ed1ba4612f5387efee06721b4e6c21cb44b1e
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2025-05-28 15:53:42 -0400

    chore: cleaned up flatten-path-data-strings

--- public/modules/vector-processing/flatten-path-data-strings.js
+++ public/modules/vector-processing/flatten-path-data-strings.js
@@ -33,8 +33,12 @@ export function flattenPathDataStrings(pathDataStrings) {
 	 * Convert flattened paths to polygon data. This takes the format
 	 * specific to https://github.com/nornagon/flatten-svg and converts it
 	 * to polygon objects.
+	 *
+	 * Note that we ignore grouping here. Our `polygons` format can contain
+	 * multiple regions in a single polygon, so we interpret all paths
+	 * as being part of the same polygon.
 	 */
-	const polygons = polygonsFromFlattenedPaths(flattenSvgResult);
+	const polygons = [{ regions: flattenSvgResult.map((entry) => entry.points) }];
 	/**
 	 * Clean up polygon data, using ClipperJS.
 	 *
@@ -66,61 +70,4 @@ export function flattenPathDataStrings(pathDataStrings) {
 	});
 	// Return the parsed and cleaned polygon data
 	return polygonsCleaned;
-}
-
-/**
- * TODO: add description and JSDOC
- * TODO: finish implementing this function
- * @returns
- */
-function polygonsFromFlattenedPaths(paths) {
-	// Group paths by groupId, each group will yield a polygon
-	// const pathsGrouped = {};
-	// for (const path of paths) {
-	// 	const { groupId } = path;
-	// 	if (!pathsGrouped[groupId]) {
-	// 		pathsGrouped[groupId] = [];
-	// 	}
-	// 	pathsGrouped[groupId].push(path);
-	// }
-	/**
-	 * TODO: use pathsGrouped rather than approach below
-	 */
-	// Initialize the polygons array
-	const polygons = [];
-
-	let regions = [];
-	let currentGroupId = null;
-	for (const path of paths) {
-		/**
-		 * TODO: is groupId still needed here?
-		 * Have not implemented any kind of group assignment...
-		 * But somehow things still seem to work?
-		 */
-		const { groupId, points } = path;
-
-		const isFirstIteration = currentGroupId === null;
-		const hasGroupId = typeof groupId === "string";
-		const hasGroupIdMatch = hasGroupId && groupId === currentGroupId;
-		if (isFirstIteration || hasGroupIdMatch) {
-			regions.push(points);
-		} else {
-			// Push the existing regions to a polygon
-			polygons.push({ regions });
-			// Reset regions, we're starting a new polygon
-			regions = [];
-			// Push the points from this path as a region in the new polygon
-			regions.push(points);
-		}
-		// Update the groupId
-		currentGroupId = groupId;
-	}
-	// Push the in-progress region (not yet pushed, cause there was no
-	// different groupID to follow it and cause it to be pushed)
-	polygons.push({ regions });
-
-	/**
-	 *
-	 */
-	return polygons;
 }
